bae966f88ceb8acc5b8a4a13929d2d7cca9eff31
Vann <tvanthey@gmail.com>
2022-07-21 23:43:27 +0700

    add guest dashboard

--- app/src/main/java/com/example/useaapp/guest/MainGuestActivity.java
+++ app/src/main/java/com/example/useaapp/guest/MainGuestActivity.java
@@ -2,15 +2,32 @@ package com.example.useaapp.guest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.example.useaapp.R;
+import com.example.useaapp.guest.home.FragmentGuestHome;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainGuestActivity extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashobard_guest);
+        NavigationBarView navigationBarView = findViewById(R.id.bottomNavigationBar);
+        getSupportFragmentManager().beginTransaction().replace(R.id.guestLayoutDashboard,new FragmentGuestHome()).commit();
+        navigationBarView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home_guest_dashboard:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.guestLayoutDashboard,new FragmentGuestHome()).commit();
+                    break;
+//                case R.id.more_guest_dashboard:
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.guestLayoutDashboard,new FragmentGuestHome()).commit();
+//                    break;
+            }
+            return true;
+        });
     }
 }
