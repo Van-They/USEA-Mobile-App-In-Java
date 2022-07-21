1af92d9cd849ffaa0d1b841f3009edb78b3211ac
Vann <tvanthey@gmail.com>
2022-07-21 20:14:23 +0700

    test

---
+++ app/src/main/java/com/example/useaapp/guest/MainGuestActivity.java
@@ -0,0 +1,16 @@
+package com.example.useaapp.guest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.useaapp.R;
+
+public class MainGuestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashobard_guest);
+    }
+}
