1fcfb40b92b541f47a65218b3cb6ae2a0cb9f05e
Vann <tvanthey@gmail.com>
2022-07-31 19:00:00 +0700

    - add about - add legal - custom log out dialog - add logo app

---
+++ app/src/main/java/com/example/useaapp/student/student_profile/ProfileSetting/About.java
@@ -0,0 +1,24 @@
+package com.example.useaapp.student.student_profile.ProfileSetting;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.useaapp.R;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = findViewById(R.id.CustomActionbarStudentAbout);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle(R.string.About);
+        TextView AboutDetail = findViewById(R.id.AboutDetail);
+        AboutDetail.setText(R.string.AboutDetail);
+    }
+}
