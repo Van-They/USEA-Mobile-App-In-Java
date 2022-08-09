c5a0cb15338ac1d57b52742e6e98ac5b92d1b513
SM Heang <mheang67@gmail.com>
2022-08-09 18:27:42 +0700

    Register

--- app/src/main/java/com/example/useaapp/guest/guest_events/GuestEvent.java
+++ app/src/main/java/com/example/useaapp/guest/guest_events/GuestEvent.java
@@ -7,11 +7,17 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.os.Bundle;
 import android.widget.TableLayout;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.useaapp.R;
 import com.example.useaapp.guest.guest_adapter.Adapter_guest_event;
 import com.example.useaapp.student.student_adapter.Adapter_studyplan;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GuestEvent extends AppCompatActivity {
 
@@ -25,6 +31,12 @@ public class GuestEvent extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         TabLayout tablayout = findViewById(R.id.tab_menu_guest_event);
         ViewPager2 pager2 = findViewById(R.id.view_pager_menu_guest_event);
+        ImageSlider slide_image = findViewById(R.id.SlideImageGuestEvent);
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.sale, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.store, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.students, ScaleTypes.FIT));
+        slide_image.setImageList(slideModels, ScaleTypes.FIT);
         String[] event_tab = {"សេចក្ដីប្រកាស", "ព្រឹត្តិការណ៍នាពេលខាងមុខ", "ព្រិត្តិការណ៍បច្ចុប្បន្ន", "ព្រិត្តិការណ៍ខាងមុនៗ"};
         pager2.setAdapter(new Adapter_guest_event(this));
         new TabLayoutMediator(tablayout,pager2,(tab, position) -> tab.setText(event_tab[position])).attach();
