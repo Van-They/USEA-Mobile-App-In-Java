ec7cb957418984a6e61953d8fd2a49105aac4ce3
Vann <tvanthey@gmail.com>
2022-08-23 19:31:28 +0700

    Cardview Event Guest

--- app/src/main/java/com/example/useaapp/student/student_schedule/StudentSchedule.java
+++ app/src/main/java/com/example/useaapp/student/student_schedule/StudentSchedule.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.example.useaapp.R;
+
+import java.util.Calendar;
 
 public class StudentSchedule extends AppCompatActivity {
     Toolbar toolbar;
@@ -17,5 +21,13 @@ public class StudentSchedule extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle(R.string.Schedule);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CalendarView calendarView = findViewById(R.id.Student_calendar);
+
+        
+
+        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            Toast.makeText(view.getContext(), "Year" +year + "Month"+month + "Day"+dayOfMonth, Toast.LENGTH_SHORT).show();
+        });
     }
 }
