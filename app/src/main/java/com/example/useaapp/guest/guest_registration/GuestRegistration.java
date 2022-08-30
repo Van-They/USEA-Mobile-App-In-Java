8df5da9200468604aa1ad5c0eb39a050c51baec0
SM Heang <mheang67@gmail.com>
2022-08-30 20:47:00 +0700

    Complete Guest Registration

--- app/src/main/java/com/example/useaapp/guest/guest_registration/GuestRegistration.java
+++ app/src/main/java/com/example/useaapp/guest/guest_registration/GuestRegistration.java
@@ -1,21 +1,44 @@
 package com.example.useaapp.guest.guest_registration;
-
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+
 import com.example.useaapp.R;
+import com.example.useaapp.guest.guest_adapter.RegistrationExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class GuestRegistration extends AppCompatActivity {
+    Toolbar toolbar;
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    HashMap<String,List<String>> expandableListDetail;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //       Start Action Bar
         setContentView(R.layout.activity_guest_registration);
-        Toolbar toolbar = findViewById(R.id.CustomActionbarGuestRegistration);
+        toolbar = findViewById(R.id.CustomActionbarGuestRegistration);
         setSupportActionBar(toolbar);
         setTitle(R.string.GuestRegistration);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        End Action Bar
+        expandableListView = findViewById(R.id.expandableListView);
 
+        expandableListDetail = RegistrationExpandableList.getData();
+
+        expandableListTitle = new ArrayList<>(expandableListDetail.keySet());
+
+        expandableListAdapter = new RegistrationExpandableListAdapter
+                (this,expandableListTitle,expandableListDetail);
+
+        expandableListView.setAdapter(expandableListAdapter);
     }
 }
