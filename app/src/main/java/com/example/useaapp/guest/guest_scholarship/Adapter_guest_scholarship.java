f4e0a255f3d4b20fa7768ab55a93020c4ec69b98
SM Heang <mheang67@gmail.com>
2022-09-30 20:31:52 +0700

    modified onclicklisener scholarship

--- app/src/main/java/com/example/useaapp/guest/guest_scholarship/Adapter_guest_scholarship.java
+++ app/src/main/java/com/example/useaapp/guest/guest_scholarship/Adapter_guest_scholarship.java
@@ -41,25 +41,25 @@ public class Adapter_guest_scholarship extends RecyclerView.Adapter<Adapter_gues
          holder.Month_guest_scholarship.setText(data.get(position).getMonth());
          holder.Year_guest_scholarship.setText(data.get(position).getYear());
 
-         holder.ReadMore_guest_scholarship.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent intent = new Intent(context, Detail_guest_scholarship.class);
-                 intent.putExtra("University_guest_scholarship", data.get(position).getUsea_name());
-                 intent.putExtra("Detail_description_guest_scholarship", data.get(position).getDetail_description());
-                 intent.putExtra("Date_open_detail_guest_scholarship", data.get(position).getApp_open_date());
-                 intent.putExtra("Date_close_detail_guest_scholarship", data.get(position).getApp_open_final_day_date());
-                 intent.putExtra("Learnmore_guest_scholarship", data.get(position).getLearnmore_link());
-                 intent.putExtra("Application_required_detail_guest_scholarship", data.get(position).getApp_required());
-                 intent.putExtra("How_to_apply_detail_guest_scholarship", data.get(position).getHow_to_apply_link());
-                 intent.putExtra("Benefit_type_detail_guest_scholarship", data.get(position).getSingle_pay());
-                 intent.putExtra("Full_benefit_detail_guest_scholarship", data.get(position).getFull_ben_detail_link());
-                 intent.putExtra("Citizenship_requirement_detail_guest_scholarship", data.get(position).getCitizen_require_detail());
-                 intent.putExtra("Total_value_detail_guest_scholarship", data.get(position).getTotal_value_price());
-                 intent.putExtra("Application_study_areas_detail_guest_scholarship", data.get(position).getApplicable_study_areas_detail());
-                 context.startActivity(intent);
-             }
-         });
+//         holder.ReadMore_guest_scholarship.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View v) {
+//                 Intent intent = new Intent(context, Detail_guest_scholarship.class);
+//                 intent.putExtra("University_guest_scholarship", data.get(position).getUsea_name());
+//                 intent.putExtra("Detail_description_guest_scholarship", data.get(position).getDetail_description());
+//                 intent.putExtra("Date_open_detail_guest_scholarship", data.get(position).getApp_open_date());
+//                 intent.putExtra("Date_close_detail_guest_scholarship", data.get(position).getApp_open_final_day_date());
+//                 intent.putExtra("Learnmore_guest_scholarship", data.get(position).getLearnmore_link());
+//                 intent.putExtra("Application_required_detail_guest_scholarship", data.get(position).getApp_required());
+//                 intent.putExtra("How_to_apply_detail_guest_scholarship", data.get(position).getHow_to_apply_link());
+//                 intent.putExtra("Benefit_type_detail_guest_scholarship", data.get(position).getSingle_pay());
+//                 intent.putExtra("Full_benefit_detail_guest_scholarship", data.get(position).getFull_ben_detail_link());
+//                 intent.putExtra("Citizenship_requirement_detail_guest_scholarship", data.get(position).getCitizen_require_detail());
+//                 intent.putExtra("Total_value_detail_guest_scholarship", data.get(position).getTotal_value_price());
+//                 intent.putExtra("Application_study_areas_detail_guest_scholarship", data.get(position).getApplicable_study_areas_detail());
+//                 context.startActivity(intent);
+//             }
+//         });
 
     }
 
@@ -68,7 +68,7 @@ public class Adapter_guest_scholarship extends RecyclerView.Adapter<Adapter_gues
         return data.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView Subject_guest_scholarship, University_guest_scholarship, Degree_guest_scholarship,
                 Fee_guest_scholarship, Date_guest_scholarship, Month_guest_scholarship, Year_guest_scholarship,
@@ -76,7 +76,6 @@ public class Adapter_guest_scholarship extends RecyclerView.Adapter<Adapter_gues
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
-
             Subject_guest_scholarship = itemView.findViewById(R.id.Subject_guest_scholarship);
             University_guest_scholarship = itemView.findViewById(R.id.University_guest_scholarship);
             Degree_guest_scholarship = itemView.findViewById(R.id.Degree_guest_scholarship);
@@ -85,7 +84,27 @@ public class Adapter_guest_scholarship extends RecyclerView.Adapter<Adapter_gues
             Month_guest_scholarship = itemView.findViewById(R.id.Month_guest_scholarship);
             Year_guest_scholarship = itemView.findViewById(R.id.Year_guest_scholarship);
             ReadMore_guest_scholarship = itemView.findViewById(R.id.ReadMore_guest_scholarship);
+            ReadMore_guest_scholarship.setOnClickListener(this);
         }
 
+        @Override
+        public void onClick(View view) {
+            int position = this.getAdapterPosition();
+            Intent intent = new Intent(context, Detail_guest_scholarship.class);
+            intent.putExtra("University_guest_scholarship", data.get(position).getUsea_name());
+            intent.putExtra("Detail_description_guest_scholarship", data.get(position).getDetail_description());
+            intent.putExtra("Date_open_detail_guest_scholarship", data.get(position).getApp_open_date());
+            intent.putExtra("Date_close_detail_guest_scholarship", data.get(position).getApp_open_final_day_date());
+            intent.putExtra("Learnmore_guest_scholarship", data.get(position).getLearnmore_link());
+            intent.putExtra("Application_required_detail_guest_scholarship", data.get(position).getApp_required());
+            intent.putExtra("How_to_apply_detail_guest_scholarship", data.get(position).getHow_to_apply_link());
+            intent.putExtra("Benefit_type_detail_guest_scholarship", data.get(position).getSingle_pay());
+            intent.putExtra("Full_benefit_detail_guest_scholarship", data.get(position).getFull_ben_detail_link());
+            intent.putExtra("Citizenship_requirement_detail_guest_scholarship", data.get(position).getCitizen_require_detail());
+            intent.putExtra("Total_value_detail_guest_scholarship", data.get(position).getTotal_value_price());
+            intent.putExtra("Application_study_areas_detail_guest_scholarship", data.get(position).getApplicable_study_areas_detail());
+            context.startActivity(intent);
+
+        }
     }
 }
